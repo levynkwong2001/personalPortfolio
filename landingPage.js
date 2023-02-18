366122cd9dcfbf8107a0a18c60924209fa9ec499
levynkhs <96076166+levynkwong2001@users.noreply.github.com>
2023-02-18 04:13:16 +0400

    put hidden window

--- landingPage.js
+++ landingPage.js
@@ -1,11 +1,22 @@
-const tittle = document.getElementsByClassName("tittle");
-const french = document.getElementsByClassName("french");
-const arabic = document.getElementsByClassName("arabic");
-const chinese = document.getElementsByClassName("chinese");
-const hindi = document.getElementsByClassName("hindi");
-const portuguese = document.getElementsByClassName("portuguese");
-const japanese = document.getElementsByClassName("japanese");
-const korean = document.getElementsByClassName("korean");
-
+const tittle = document.getElementById('container-skill');
 tittle.style.visibility = "hidden";
-french.style.visibility = "hidden";
+
+
+function toggle() {
+
+    if (tittle.style.visibility == "hidden") {
+        tittle.style.visibility = "visible";
+    }
+    console.log("success");
+}
+
+
+function close() {
+
+    // if (tittle.style.visibility == "visible") {
+    //     tittle.style.visibility = "hidden";
+    // }
+    console.log("success");
+}
+
+
